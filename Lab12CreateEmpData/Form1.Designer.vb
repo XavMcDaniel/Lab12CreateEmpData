68e8f75e102e2b36738e21b24ad9a53f17916a82
Xavier McDaniel <S01051073@acad.tri-c.edu>
2018-04-15 13:04:28 -0400

    Finished Assignment

--- Lab12CreateEmpData/Form1.Designer.vb
+++ Lab12CreateEmpData/Form1.Designer.vb
@@ -22,9 +22,247 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.grpEmployeeData = New System.Windows.Forms.GroupBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtExtension = New System.Windows.Forms.TextBox()
+        Me.txtTelephone = New System.Windows.Forms.TextBox()
+        Me.cboDepartment = New System.Windows.Forms.ComboBox()
+        Me.txtEmployeeNumber = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtMiddleName = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.grpEmployeeData.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'grpEmployeeData
+        '
+        Me.grpEmployeeData.Controls.Add(Me.txtEmail)
+        Me.grpEmployeeData.Controls.Add(Me.txtExtension)
+        Me.grpEmployeeData.Controls.Add(Me.txtTelephone)
+        Me.grpEmployeeData.Controls.Add(Me.cboDepartment)
+        Me.grpEmployeeData.Controls.Add(Me.txtEmployeeNumber)
+        Me.grpEmployeeData.Controls.Add(Me.txtLastName)
+        Me.grpEmployeeData.Controls.Add(Me.txtMiddleName)
+        Me.grpEmployeeData.Controls.Add(Me.txtFirstName)
+        Me.grpEmployeeData.Controls.Add(Me.Label8)
+        Me.grpEmployeeData.Controls.Add(Me.Label7)
+        Me.grpEmployeeData.Controls.Add(Me.Label6)
+        Me.grpEmployeeData.Controls.Add(Me.Label5)
+        Me.grpEmployeeData.Controls.Add(Me.Label4)
+        Me.grpEmployeeData.Controls.Add(Me.Label3)
+        Me.grpEmployeeData.Controls.Add(Me.Label2)
+        Me.grpEmployeeData.Controls.Add(Me.Label1)
+        Me.grpEmployeeData.Location = New System.Drawing.Point(12, 12)
+        Me.grpEmployeeData.Name = "grpEmployeeData"
+        Me.grpEmployeeData.Size = New System.Drawing.Size(273, 259)
+        Me.grpEmployeeData.TabIndex = 0
+        Me.grpEmployeeData.TabStop = False
+        Me.grpEmployeeData.Text = "Enter Employee Data"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(127, 218)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(121, 20)
+        Me.txtEmail.TabIndex = 15
+        '
+        'txtExtension
+        '
+        Me.txtExtension.Location = New System.Drawing.Point(127, 192)
+        Me.txtExtension.Name = "txtExtension"
+        Me.txtExtension.Size = New System.Drawing.Size(121, 20)
+        Me.txtExtension.TabIndex = 14
+        '
+        'txtTelephone
+        '
+        Me.txtTelephone.Location = New System.Drawing.Point(127, 166)
+        Me.txtTelephone.Name = "txtTelephone"
+        Me.txtTelephone.Size = New System.Drawing.Size(121, 20)
+        Me.txtTelephone.TabIndex = 13
+        '
+        'cboDepartment
+        '
+        Me.cboDepartment.FormattingEnabled = True
+        Me.cboDepartment.Items.AddRange(New Object() {"Accounting", "Administration", "Marketing", "MIS", "Sales"})
+        Me.cboDepartment.Location = New System.Drawing.Point(127, 139)
+        Me.cboDepartment.Name = "cboDepartment"
+        Me.cboDepartment.Size = New System.Drawing.Size(121, 21)
+        Me.cboDepartment.TabIndex = 12
+        Me.cboDepartment.Text = "Accounting"
+        '
+        'txtEmployeeNumber
+        '
+        Me.txtEmployeeNumber.Location = New System.Drawing.Point(127, 113)
+        Me.txtEmployeeNumber.Name = "txtEmployeeNumber"
+        Me.txtEmployeeNumber.Size = New System.Drawing.Size(121, 20)
+        Me.txtEmployeeNumber.TabIndex = 11
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(127, 87)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(121, 20)
+        Me.txtLastName.TabIndex = 10
+        '
+        'txtMiddleName
+        '
+        Me.txtMiddleName.Location = New System.Drawing.Point(127, 61)
+        Me.txtMiddleName.Name = "txtMiddleName"
+        Me.txtMiddleName.Size = New System.Drawing.Size(121, 20)
+        Me.txtMiddleName.TabIndex = 9
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(127, 35)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(121, 20)
+        Me.txtFirstName.TabIndex = 8
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(42, 221)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(79, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "E-mail Address:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(65, 195)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(56, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Extension:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(60, 169)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Telephone:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(56, 142)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Department:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Employee Number"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Last Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(49, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Middle Name:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(61, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "First Name:"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(12, 289)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(79, 31)
+        Me.btnSave.TabIndex = 1
+        Me.btnSave.Text = "&Save Record"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(108, 289)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(79, 31)
+        Me.btnClear.TabIndex = 2
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(206, 289)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(79, 31)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(301, 335)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.grpEmployeeData)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.grpEmployeeData.ResumeLayout(False)
+        Me.grpEmployeeData.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents grpEmployeeData As GroupBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtExtension As TextBox
+    Friend WithEvents txtTelephone As TextBox
+    Friend WithEvents cboDepartment As ComboBox
+    Friend WithEvents txtEmployeeNumber As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtMiddleName As TextBox
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
 End Class
